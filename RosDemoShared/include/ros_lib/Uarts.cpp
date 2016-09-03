fd072be17d2ae2e1c21c13e9c3f77513fdb63fcb
unknown <cfharr@gmail.com>
2016-09-03 11:32:00 -0700

    Create RosDemoShared include directory

---
+++ RosDemoShared/include/ros_lib/Uarts.cpp
@@ -0,0 +1,70 @@
+/* ========================================
+ * 
+ * Host USB CDC Uart for HiveBio Ministat on PSoC5
+ * 
+ * Copyright C. Harrison
+ * BSD 2-clause license http://opensource.org/licenses/BSD-2-Clause
+ *
+ */
+#include "ros.h"
+
+#include "Uarts.h"
+extern "C" {
+  #include "project.h"
+}
+
+void Uart::begin(unsigned long baud) {
+  // baud rate unused
+  USBUART_Start(0, USBUART_DWR_VDDD_OPERATION);
+  while(!USBUART_GetConfiguration());
+  USBUART_CDC_Init();
+  read_ptr = read_tail = read_buf;
+};
+
+int Uart::read(void) {
+  int32_t data = -1;
+  uint16_t count;
+  if (read_ptr<read_tail) {
+    data = *read_ptr++;
+  } else if ((count = USBUART_GetCount()) != 0) {
+    // data is ready
+    if (count > sizeof(read_buf)) {
+      count = sizeof(read_buf);
+    }
+    read_ptr = read_buf;
+    read_tail = read_buf + USBUART_GetData(read_buf, count);
+    if (read_ptr<read_tail) {
+      data = *read_ptr++;
+    }
+    P1_6_Write(1);
+  }
+  return data;
+};
+
+size_t Uart::write(uint8_t data) {
+  while(USBUART_CDCIsReady() == 0u);    /* Wait till component is ready to send more data */ 
+  USBUART_PutChar(data);
+  return 1;
+};
+
+size_t Uart::write(uint8_t* data, int length) {
+  int buf_size = USBUART_EP[USBUART_cdc_data_in_ep].bufferSize;
+  while(1) {
+    while(USBUART_CDCIsReady() == 0u);    /* Wait till component is ready to send more data */
+    if (length >= buf_size) {
+      USBUART_PutData(data, buf_size);
+      length -= buf_size;
+      data += buf_size;
+    } else {
+      USBUART_PutData(data, length);
+      break;
+    }
+  }
+  return 1;
+};
+
+
+
+
+Uart Uart0;
+
